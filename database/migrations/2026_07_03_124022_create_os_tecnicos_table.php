1f4ba117f071ab14c3c711a32eca86525ac07596
Matheus Sousa <matheussousa092006@gmail.com>
2026-07-03 15:34:01 -0300

    ajustando o nome create_os_tecnicos

--- database/migrations/2026_07_03_124022_create_os_tecnicos_table.php
+++ database/migrations/2026_07_03_124022_create_os_tecnicos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('os_tecnico', function (Blueprint $table) {
+        Schema::create('os_tecnicos', function (Blueprint $table) {
             $table->id();
             
             $table->unsignedBigInteger('task_id'); 
