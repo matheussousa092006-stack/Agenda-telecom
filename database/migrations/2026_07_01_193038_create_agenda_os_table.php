f61ff72354c9b02fb1a3b51895e6f25c2249ae68
Matheus Sousa <matheussousa092006@gmail.com>
2026-07-03 09:36:54 -0300

    Cria estrutura inicial do banco da agenda

---
+++ database/migrations/2026_07_01_193038_create_agenda_os_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agenda_os', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('os_tecnico_id');
+
+            $table->date('data');
+
+            $table->time('hora_inicio')->nullable();
+            $table->time('hora_fim')->nullable();
+
+            $table->integer('ordem')->default(0);
+
+            $table->text('observacao')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agenda_os');
+    }
+};
---
+++ database/migrations/2026_07_01_205451_create_os_tecnico_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('os_tecnico', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('task_id'); 
+            $table->unsignedBigInteger('parent_task_id')->nullable();
+        
+            $table->string('tecnico_nome')->nullable();
+            $table->string('ordem_servico')->nullablea();
+            $table->string('titulo')->nullable();
+            $table->string('task_code')->nullable();
+            $table->string('categoria')->nullable();
+            $table->string('regiao')->nullable();
+            $table->string('status')->nullable();
+            $table->string('protocolo')->nullable();
+            $table->string('prioridade')->nullable();
+            $table->dateTime('data_criacao')->nullable();
+            $table->dateTime('data_conclusao')->nullable();
+            $table->dateTime('criada_em')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('os_tecnico');
+    }
+};
