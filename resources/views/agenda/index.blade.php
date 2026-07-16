8c7c65bfa866958a4ba78fd5b363a019908a5209
Matheus Sousa <matheussousa092006@gmail.com>
2026-07-16 16:31:38 -0300

    Adição cabeçalho

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -14,9 +14,87 @@
             background:#f4f6f9;
         }
 
+        .cabecalho{
+
+    text-align:center;
+
+    padding:25px 0 10px;
+
+}
+
+        .cabecalho h1{
+
+    margin:0;
+
+    font-size:44px;
+
+    color:#1f2937;
+
+}
+
+        .cabecalho p{
+
+    margin-top:8px;
+
+    color:#6b7280;
+
+    font-size:16px;
+
+}
+
         h1{
             margin:20px;
         }
+
+        .header-agenda{
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    gap:20px;
+    margin-bottom:20px;
+
+}
+
+.btn-data{
+
+    width:45px;
+    height:45px;
+
+    display:flex;
+    align-items:center;
+    justify-content:center;
+
+    background:#2563eb;
+    color:white;
+
+    text-decoration:none;
+
+    border-radius:50%;
+
+    font-size:22px;
+    font-weight:bold;
+
+    transition:.2s;
+
+}
+
+.btn-data:hover{
+
+    background:#1d4ed8;
+
+}
+
+.data-atual{
+
+    font-size:28px;
+    font-weight:bold;
+    color:#222;
+
+    min-width:180px;
+    text-align:center;
+
+}
 
         .agenda{
             display:flex;
@@ -83,27 +161,57 @@
 
 <body>
 
-<h1>Agenda Telecom</h1>
+<div class="cabecalho">
 
-<div class="flex items-center justify-center gap-4 mb-6">
+    <h1>Agenda Telecom</h1>
+
+    <p>Programação diária dos técnicos</p>
+
+</div>
+
+
+<div class="header-agenda">
+
     <a
-        href="/?data={{ \Carbon\Carbon::parse($dataSelecionada)->subDay()->toDateString() }}"
-        class="px-3 py-2 bg-gray-200 rounded hover:bg-gray-300"
+        class="btn-data"
+        href="/?data={{ \Carbon\Carbon::parse($dataSelecionada)->copy()->subDay()->toDateString() }}"
     >
         ◀
     </a>
 
-    <div class="text-xl font-semibold">
-        {{ \Carbon\Carbon::parse($dataSelecionada)->format('d/m/Y') }}
+    <div class="data-atual">
+
+    {{ \Carbon\Carbon::parse($dataSelecionada)->format('d/m/Y') }}
+
+    <div class="dia-semana">
+
+        {{ \Carbon\Carbon::parse($dataSelecionada)->translatedFormat('l') }}
+
+       .dia-semana{
+
+       margin-top:8px;
+
+       font-size:15px;
+
+       color:#6b7280;
+
+       font-weight:normal;
+
+     }
+
+
     </div>
 
+</div>
     <a
-        href="/?data={{ \Carbon\Carbon::parse($dataSelecionada)->addDay()->toDateString() }}"
-        class="px-3 py-2 bg-gray-200 rounded hover:bg-gray-300"
+        class="btn-data"
+        href="/?data={{ \Carbon\Carbon::parse($dataSelecionada)->copy()->addDay()->toDateString() }}"
     >
         ▶
     </a>
+
 </div>
+
 
 <div class="agenda">
 
