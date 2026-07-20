fcb663865dff814c07d308677b578b36511efbb5
Matheus Sousa <matheussousa092006@gmail.com>
2026-07-20 11:11:58 -0300

    Evita duplicidade de OS no seeder da agenda

--- database/seeders/AgendaOsSeeder.php
+++ database/seeders/AgendaOsSeeder.php
@@ -2,42 +2,45 @@
 
 namespace Database\Seeders;
 
+use App\Models\AgendaOs;
+use App\Models\OsTecnico;
+use App\Models\Tecnico;
 use Illuminate\Database\Seeder;
 
 class AgendaOsSeeder extends Seeder
 {
     public function run(): void
     {
-        \App\Models\AgendaOs::firstOrCreate(
-
+        $programacoes = [
             [
-                'os_tecnico_id' => 1,
-                'tecnico_id'    => 1,
-                'data'          => now()->toDateString(),
-            ],
-
-            [
-                'hora_inicio' => '08:00',
-                'hora_fim'    => '10:00',
-                'ordem'       => 1,
-                'observacao'  => null,
-            ]
-        );
-
-        \App\Models\AgendaOs::firstOrCreate(
-
-            [
-                'os_tecnico_id' => 2,
-                'tecnico_id'    => 2,
-                'data'          => now()->toDateString(),
-            ],
-
-            [
+                'ordem_servico' => 'OS-1001',
+                'tecnico' => 'Eduardo',
                 'hora_inicio' => '09:00',
-                'hora_fim'    => '11:00',
-                'ordem'       => 1,
-                'observacao'  => null,
-            ]
-        );
+                'hora_fim' => '12:30',
+            ],
+            [
+                'ordem_servico' => 'OS-1002',
+                'tecnico' => 'Arrhenius',
+                'hora_inicio' => '06:00',
+                'hora_fim' => '07:30',
+            ],
+        ];
+
+        foreach ($programacoes as $programacao) {
+            $osTecnico = OsTecnico::where('ordem_servico', $programacao['ordem_servico'])->firstOrFail();
+            $tecnico = Tecnico::where('nome', $programacao['tecnico'])->firstOrFail();
+
+            AgendaOs::firstOrCreate(
+                ['os_tecnico_id' => $osTecnico->id],
+                [
+                    'tecnico_id' => $tecnico->id,
+                    'data' => now()->toDateString(),
+                    'hora_inicio' => $programacao['hora_inicio'],
+                    'hora_fim' => $programacao['hora_fim'],
+                    'ordem' => 1,
+                    'observacao' => null,
+                ],
+            );
+        }
     }
 }
